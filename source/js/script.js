ff0f4e8315b329e80c69a36baac50cd595bafa7e
Selena Strain <selena@gravityworksdesign.com>
2018-06-29 14:21:17 -0400

    Removed button ripple and Frank McKenna image, added svg body ajax to script.js and adjusted the formatting

--- source/js/script.js
+++ source/js/script.js
@@ -1,34 +1,13 @@
 'use strict';
 
-/* 
+/*
  * !!! Change this for Drupal ¡¡¡
  * Any script file in a Drupal theme, like Basic, should have the functions you need
  */
 (function($) {
   $(document).ready(function(){
 
-  		console.log('the script file is here');
-
-  		// Button Ripple
-		$('.js-ripple').on('click', function(e) {
-			console.log('you clicked a ripple button!');
-			var target = e.target;
-			var rect = target.getBoundingClientRect();
-			var ripple = target.querySelector('.ripple');
-			if (!ripple) {
-			    ripple = document.createElement('span');
-			    ripple.className = 'ripple';
-			    ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-			    target.appendChild(ripple);
-			}
-			ripple.classList.remove('show');
-			var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-			var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-			ripple.style.top = top + 'px';
-			ripple.style.left = left + 'px';
-			ripple.classList.add('show');
-			return false;
-		});
+  	console.log('the script file is here');
 
 		// Get IE Version
 		var version = detectIE();
@@ -41,42 +20,50 @@
 		// Detect IE
 		// returns version of IE, or false if browser is not Internet Explorer
 		function detectIE() {
-		var ua = window.navigator.userAgent;
+			var ua = window.navigator.userAgent;
 
-		var trident = ua.indexOf('Trident/');
-		if (trident > 0) {
-			// IE 11 => return version number
-			var rv = ua.indexOf('rv:');
-			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+			var trident = ua.indexOf('Trident/');
+			if (trident > 0) {
+				// IE 11 => return version number
+				var rv = ua.indexOf('rv:');
+				return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+			}
+
+			var edge = ua.indexOf('Edge/');
+			if (edge > 0) {
+				// Edge (IE 12+) => return version number
+				return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+			}
+			// other browser
+			return false;
 		}
 
-		var edge = ua.indexOf('Edge/');
-		if (edge > 0) {
-			// Edge (IE 12+) => return version number
-			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-		}
-		// other browser
-		return false;
-	}
+		// Skip Nav with tabindex fix
+		$(".skip-link").click(function(event){
 
-	// Skip Nav with tabindex fix
-	$(".skip-link").click(function(event){
+			// strip the leading hash and declare
+			// the content we're skipping to
+			var skipTo="#"+this.href.split('#')[1];
 
-		// strip the leading hash and declare
-		// the content we're skipping to
-		var skipTo="#"+this.href.split('#')[1];
+			// Setting 'tabindex' to -1 takes an element out of normal
+			// tab flow but allows it to be focused via javascript
+			$(skipTo).attr('tabindex', -1).on('blur focusout', function () {
 
-		// Setting 'tabindex' to -1 takes an element out of normal
-		// tab flow but allows it to be focused via javascript
-		$(skipTo).attr('tabindex', -1).on('blur focusout', function () {
+				// when focus leaves this element,
+				// remove the tabindex attribute
+				$(this).removeAttr('tabindex');
 
-			// when focus leaves this element,
-			// remove the tabindex attribute
-			$(this).removeAttr('tabindex');
+			}).focus(); // focus on the content container
+		});
 
-		}).focus(); // focus on the content container
-	});
-
+		// Add SVG to body
+		$.ajax({
+			// Update url as needed
+			url: "/themes/themename/images/svg-symbols.svg",
+			context: document.body
+		}).done(function (data) {
+			$('svg', data).attr('class', 'visually-hidden').prependTo('body');
+		});
 
 	});
 })(jQuery);
