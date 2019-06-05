507af3ad624a6aba360b7761160d9f8826032ba4
Kurt Trowbridge <kurt@gravityworksdesign.com>
2019-06-04 23:20:45 -0400

    Removed unneeded console.log and IE detection from script.js

--- source/js/script.js
+++ source/js/script.js
@@ -7,37 +7,6 @@
 (function($) {
   $(document).ready(function(){
 
-  	console.log('the script file is here');
-
-		// Get IE Version
-		var version = detectIE();
-
-		// import stylesheet for IE 11 and less
-		if (version !== false && version <= 11) {
-			$('head').append('<link rel="stylesheet" type="text/css" href="/sites/all/themes/raind/css/lte-ie11.css">');
-		}
-
-		// Detect IE
-		// returns version of IE, or false if browser is not Internet Explorer
-		function detectIE() {
-			var ua = window.navigator.userAgent;
-
-			var trident = ua.indexOf('Trident/');
-			if (trident > 0) {
-				// IE 11 => return version number
-				var rv = ua.indexOf('rv:');
-				return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-			}
-
-			var edge = ua.indexOf('Edge/');
-			if (edge > 0) {
-				// Edge (IE 12+) => return version number
-				return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-			}
-			// other browser
-			return false;
-		}
-
 		// Skip Nav with tabindex fix
 		$(".skip-link").click(function(event){
 
